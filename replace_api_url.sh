6128ee494097bebbad4f7d92d73ea63d18a75f55
samwood2011 <samwood1989@gmail.com>
2022-03-25 09:01:15 +0000

    Update replace_api_url.sh

--- replace_api_url.sh
+++ replace_api_url.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-WF_SERVER_DEFAULT="http://localhost:8080/api"; 
+WF_SERVER_DEFAULT="http://localhost:8080/"; 
 OAUTH_ENABLED_DEFAULT="N";
 OAUTH_TOKEN_URL_DEFAULT="http://localhost:9990/auth/realms/conductor/protocol/openid-connect/token";
 OAUTH_USER_INFO_URL_DEFAULT="http://localhost:8080/userinfo";
